ce4d71f0abc704c24f9c86cb5c3f71e378301cf0
YussifKahilo <yusufkahilo@gmail.com>
2022-07-26 22:39:10 +0200

    How to page added and updates for adaption page

--- lib/pages/how_to.dart
+++ lib/pages/how_to.dart
@@ -1,141 +1,71 @@
 import 'package:flutter/material.dart';
-import 'package:petology_test/data/models/category.dart';
 import 'package:petology_test/wedgits/footer.dart';
 
-class AdaptionPage extends StatelessWidget {
-  AdaptionPage({Key? key}) : super(key: key);
-
-  String? dropdownvalue = '';
-
-  var items = [
-    'Pet',
-    'Dog',
-    'Cat',
-    'Toto',
-  ];
-
-  List<Widget> pets = [];
+class HowToPage extends StatelessWidget {
+  HowToPage({Key? key}) : super(key: key);
 
   List _isHovering = [false, false, false, false];
 
   @override
   Widget build(BuildContext context) {
-    var screenSize = MediaQuery.of(context).size;
-    pets = [
-      petCard(),
-      petCard(),
-      petCard(),
-      petCard(),
-      petCard(),
-    ];
+    Size screenSize = MediaQuery.of(context).size;
 
-    List<Profile> Profiles = [];
-    Profiles.add(new Profile("Dogs", "ic_cat.png", false));
-    Profiles.add(new Profile("Cats", "ic_dog.png", false));
+    String title = "How do you feed your dog ?";
+    String body =
+        "How do you feed your dog ? How do you feed your dog ? How do you feed your dog ? How do you feed your dog ? How do you feed your dog ? How do you feed your dog ? How do you feed your dog ? How do you feed your dog ? How do you feed your dog ? How do you feed your dog ?";
 
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: navBar(screenSize),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            Stack(
-              children: [
-                Positioned(
-                  left: 100,
-                  top: 200,
-                  child: Image.asset("doghand.png"),
-                  width: 250,
-                  height: 250,
+            Container(
+                height: screenSize.height,
+                width: screenSize.width,
+                decoration: const BoxDecoration(
+                    gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    Color.fromRGBO(103, 71, 57, 1),
+                    Color.fromRGBO(24, 7, 1, 1),
+                  ],
+                )),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Image.asset(
+                      "doghand.png",
+                      width: 500,
+                      height: 500,
+                    ),
+                  ],
+                )),
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Container(
+                width: screenSize.width,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      title,
+                      style: TextStyle(
+                          color: Color(0xff492f24),
+                          fontSize: 40,
+                          fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.left,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Text(body,
+                          style:
+                              TextStyle(color: Colors.grey[900], fontSize: 20)),
+                    )
+                  ],
                 ),
-                Positioned(
-                    right: 100,
-                    top: 350,
-                    child: Image.asset("doghand.png"),
-                    width: 250,
-                    height: 250),
-                Positioned(
-                    top: 500,
-                    child: Image.asset("doghand.png"),
-                    width: 250,
-                    height: 250),
-                Positioned(
-                    bottom: 50,
-                    left: 100,
-                    child: Image.asset("doghand.png"),
-                    width: 250,
-                    height: 250),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            filterSection("Breed", screenSize.width / 7),
-                            filterSection("Age", screenSize.width / 7),
-                            filterSection("Size", screenSize.width / 7),
-                            filterSection("Good With", screenSize.width / 7)
-                          ],
-                        ),
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            filterSection("Gender", screenSize.width / 7),
-                            filterSection("Color", screenSize.width / 7),
-                            filterSection("Hair Lenght", screenSize.width / 7),
-                            filterSection(
-                                "Care & Behavior", screenSize.width / 7)
-                          ],
-                        ),
-                      ),
-                      Column(
-                        children: [
-                          Container(
-                            height: 450,
-                            child: ListView.builder(
-                              shrinkWrap: true,
-                              scrollDirection: Axis.horizontal,
-                              itemCount: pets.length >= 3 ? 3 : pets.length,
-                              itemBuilder: (context, index) {
-                                return petCard();
-                              },
-                            ),
-                          ),
-                          Container(
-                            height: pets.length - 3 > 0 ? 450 : 0,
-                            child: ListView.builder(
-                              shrinkWrap: true,
-                              scrollDirection: Axis.horizontal,
-                              itemCount:
-                                  pets.length - 3 > 0 ? pets.length - 3 : 0,
-                              itemBuilder: (context, index) {
-                                return petCard();
-                              },
-                            ),
-                          ),
-                        ],
-                      ),
-                      Padding(
-                        padding: const EdgeInsets.all(20.0),
-                        child: InkWell(
-                          child: Text(
-                            "Show More...",
-                            style: TextStyle(
-                                color: Color(0xff492f24),
-                                fontSize: 20,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
+              ),
             ),
             CustomFooter()
           ],
@@ -148,15 +78,7 @@ class AdaptionPage extends StatelessWidget {
     return PreferredSize(
       preferredSize: Size(screenSize.width, 1000),
       child: Container(
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(
-          begin: Alignment.topLeft,
-          end: Alignment.bottomRight,
-          colors: [
-            Color.fromRGBO(103, 71, 57, 1),
-            Color.fromRGBO(24, 7, 1, 1),
-          ],
-        )),
+        color: Colors.transparent,
         child: Padding(
           padding: EdgeInsets.all(20),
           child: Row(
@@ -338,92 +260,6 @@ class AdaptionPage extends StatelessWidget {
               ),
             ],
           ),
-        ),
-      ),
-    );
-  }
-
-  Widget petCard() {
-    return Container(
-      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
-      width: 300,
-      decoration: BoxDecoration(
-          color: Color(0xffeaeaea), borderRadius: BorderRadius.circular(30)),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          SizedBox(
-            child: Image.asset("dog.png"),
-            height: 200,
-          ),
-          Text(
-            "Name",
-            style: TextStyle(
-                fontSize: 20, fontWeight: FontWeight.bold, color: Colors.brown),
-          ),
-          GestureDetector(
-            onTap: () {},
-            child: Container(
-              alignment: Alignment.center,
-              width: 150,
-              height: 40,
-              decoration: BoxDecoration(
-                color: Color(0xff492f24),
-                borderRadius: BorderRadius.circular(25),
-              ),
-              child: Text("Send",
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  )),
-            ),
-          ),
-          Text(
-            "By User",
-            style: TextStyle(fontSize: 10, color: Colors.black),
-          )
-        ],
-      ),
-    );
-  }
-
-  Widget filterSection(String name, double width) {
-    return Container(
-      width: width,
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              name,
-              style: TextStyle(fontSize: 18),
-            ),
-            customDropDown()
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget customDropDown() {
-    return Card(
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(15)),
-      elevation: 5,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15.0),
-        child: DropdownButton(
-          underline: SizedBox(),
-          isExpanded: true,
-          onChanged: (String? newValue) {
-            dropdownvalue = newValue;
-          },
-          items: items.map((items) {
-            return DropdownMenuItem(
-              value: items,
-              child: Text(items),
-            );
-          }).toList(),
         ),
       ),
     );
